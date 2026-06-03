a9e9ee9a9fe091fb4677a1370af8687add03d753
winiciuspereira-byte <winicius.pereira@edu.senai.br>
2026-06-03 11:15:32 -0300

    inserção de js no codigo

---
+++ form-web/js/script.js
@@ -0,0 +1,22 @@
+/*
+      criando variáveis
+*/
+ 
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");    
+ 
+/*
+      criando função
+*/
+ 
+button.addEventListener("click", function(event)  {
+        event.preventDefault();  
+        console.log(nome.value)
+ 
+        // Incluir itens no HTML
+ 
+ 
+    });
+   
+ 
