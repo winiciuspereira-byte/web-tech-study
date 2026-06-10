ae8d82018cc3b7679132d8cb9e5224de7ef926c6
winiciuspereira-byte <winicius.pereira@edu.senai.br>
2026-06-10 09:04:10 -0300

    inserção calc2

---
+++ form-web/js/calc2.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Digite um número: "));
+let num2 = Number(prompt("Digite outro número: "));
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
