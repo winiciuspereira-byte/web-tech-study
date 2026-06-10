a37ec713f809d03ce49551d47f7f750a54d761da
winiciuspereira-byte <winicius.pereira@edu.senai.br>
2026-06-10 08:45:14 -0300

    att javascript

---
+++ form-web/js/func.js
@@ -0,0 +1,20 @@
+function minhaFuncao() {
+    document.write("Olá, esta é a minha função JavaScript!");
+}
+
+function pularlinha() {
+    document.write("<br><br>");
+}
+
+function mostrarnome(nome) {
+    pularlinha();
+    document.write(nome);
+    pularlinha();
+
+}
+
+minhaFuncao();
+pularlinha();
+minhaFuncao();  
+mostrarnome("Winicius");
+mostrarnome("Pedro po");
