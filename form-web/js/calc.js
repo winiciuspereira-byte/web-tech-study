b0377bb753d3210aebfce08f597842afcc3e80ff
WINICIUS CESAR DE SANTANA PEREIRA <winicius.pereira@senaisp.edu.br>
2026-06-17 09:09:24 -0300

    feat: add

--- form-web/js/calc.js
+++ form-web/js/calc.js
@@ -6,7 +6,7 @@ let sub = num1 - num2;
 let mult = num1 * num2;
 let div = num1 / num2;
 
-console.log("Soma: " + soma);
+console.log("Soma: " + soma)
 console.log("Subtração: " + sub);
 console.log("Multiplicação: " + mult);
-console.log("Divisão: " + div);
+console.log("Divisão: " + div)
